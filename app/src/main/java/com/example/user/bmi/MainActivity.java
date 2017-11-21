c664a9d97e41d91b99b319377942010a624ceb4e
Jason541029 <jtr5494@gmail.com>
2017-11-21 12:04:54 +0800

    onCreat

--- app/src/main/java/com/example/user/bmi/MainActivity.java
+++ app/src/main/java/com/example/user/bmi/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.user.bmi;
 
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,6 +11,7 @@ import android.widget.EditText;import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     String s = new String("abc");
+
     View.OnClickListener listener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
@@ -19,34 +21,92 @@ public class MainActivity extends AppCompatActivity {
                     .setPositiveButton("OK",null)
                     .show();
 
+
         }
     };
+    private EditText edweight;
+    private EditText edhight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button bHelp = (Button) findViewById(R.id.b_help);
+        Log.d("MainActivity", "onCreat" );
+        Button bHelp = findViews();
+        getResources().getString(R.string.app_name);
         bHelp.setOnClickListener(listener);
 
 
     }
 
+    private Button findViews() {
+        edweight = (EditText) findViewById((R.id.ed_weight);
+        edhight = (EditText) findViewById(R.id.ed_height);
+        return (Button)  findViewById(R.id.b_help);
+    }
+
+    public MainActivity() {
+        super();
+        Log.d("MainActivity", "onCreat" );
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MainActivity", "onStart" );
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MainActivity", "onStop" );
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("MainActivity", "onDestroy" );
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("MainActivity", "onPause" );
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("MainActivity", "onResume" );
+    }
+
     public void bmi(View view) {
         Log.d("MainActivity","testing bmi method");
-        EditText edHeight = (EditText) findViewById(R.id.height);
-        EditText edWeight = (EditText) findViewById(R.id.ed_weight);
-        float weight = Float.parseFloat(edWeight.getText().toString());
-        float height = Float.parseFloat(edHeight.getText().toString());
+
+        float weight = Float.parseFloat(edweight.getText().toString());
+        float height = Float.parseFloat(edhight.getText().toString());
         float bmi = weight / (height * height);
-        new AlertDialog.Builder(this)
+        Intent intent = new Intent(this, ResultActivity.class);
+        intent.putExtra("EXTRA_BMI", bmi);
+        startActivity(intent);
+
+
+       /* new AlertDialog.Builder(this)
                 .setMessage(getString(R.string.Your_BMI_is)+bmi)
                 .setMessage(getString(R.string.bmi_title))
                 .setPositiveButton(R.string.OK, null)
-                .show();
+                .show();*/
+
+
         TextView dia = (TextView)findViewById(R.id.b_bmi);// 取得 顯示診斷 物件
 
-        if (bmi<20)
+        if (bmi<20) {
+            new AlertDialog.Builder(this)
+                    .setMessage("Your BMIis" + bmi)
+                    .setTitle("多吃")
+                    .setPositiveButton("OK",null)
+                    .show();
+        }
             dia.setText("請多吃點");
         else if (20<= bmi && bmi< 24)
             dia.setText("正常範圍");
@@ -69,4 +129,3 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-}
